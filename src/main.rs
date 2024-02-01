0c98c73133a79dbc7a602f8147145b583d8cdee2
Simojoviz <simonejov@gmail.com>
2024-02-01 15:55:19 +0100

    Starting BLE-based authentication

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+mod ble_auth;
+
+use p256::{EncodedPoint, ecdh::EphemeralSecret};
+use rand_core::OsRng; // requires 'getrandom' feature
+
 fn main() {
-    println!("Hello, world!");
+
+
+    // Alice
+    let alice_sk = EphemeralSecret::random(&mut OsRng);
+    let alice_pk = alice_sk.public_key();
+    let alice_pk_bytes = EncodedPoint::from(alice_pk);
+
+    // Bob
+    let bob_sk = EphemeralSecret::random(&mut OsRng);
+    let bob_pk = bob_sk.public_key();
+    let bob_pk_bytes = EncodedPoint::from(bob_pk);
+
+    let alice_shared = alice_sk.diffie_hellman(&bob_pk);
+
+    let bob_shared = bob_sk.diffie_hellman(&alice_pk);
+
+    // Use the shared secret for further cryptographic operations
+    println!("Alice Shared: {:?}", alice_shared.raw_secret_bytes());
+    println!("Bob Shared:   {:?}", bob_shared.raw_secret_bytes());
 }
