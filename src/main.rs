9c57e0766bb212e348a8839512434ef92b5f0fca
Mario Gottardo <mario.gottardo27@gmail.com>
2024-02-13 12:44:47 +0100

    minimal cli structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::{process, str};
+use inquire::Select;
+
+fn handle_command(command: &str) {
+    match command {
+        "scan" => println!("Not implemented yet"),
+        "pair" => println!("Not implemented yet"),
+        "exit" => {print!("exiting"); process::exit(0)},
+        _ => println!("error")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("YAFTT - SN shell");
+
+    loop {
+        let command = Select::new("Select a command:", vec!["scan", "pair", "exit"]).prompt();
+        
+        match command {
+            Ok(command) => handle_command(command),
+            Err(_) => {println!("An error occurred, exiting"); process::exit(1)},
+        }
+    }
+
 }
