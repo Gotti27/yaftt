ae40fbb716bbfe51a7ff225848a5d4210afef189
Simojoviz <simonejov@gmail.com>
2024-02-19 11:46:36 +0100

    scan method implementation starded

--- src/ble_auth.rs
+++ src/ble_auth.rs
@@ -1,25 +1,59 @@
+use std::process;
+use bluer::{Session, AdapterEvent, Result};
+use async_trait::async_trait;
+use futures::{pin_mut, StreamExt};
+use colored::Colorize;
+
 // Abstract view of a Bluetooth Low Energy authenticator
+#[async_trait]
 pub trait BLEAuthenticator {
-    fn scan(&self);
-    fn pair(&self);
+    async fn scan(&self) -> Result<()>;
+    async fn pair(&self);
 }
 
 // Bluetooth Low Energy authenticator for Linux system
-struct LinuxBLEAuthenticator { }
+struct LinuxBLEAuthenticator {
+    session: Session,
+}
 
+#[async_trait]
 impl BLEAuthenticator for LinuxBLEAuthenticator {
-    fn scan(&self) {
-        todo!()
+    async fn scan(&self) -> Result<()>{
+        let adapter = self.session.default_adapter().await?;
+
+        adapter.set_powered(true).await?;
+
+        let discover = adapter.discover_devices().await?;
+        pin_mut!(discover);
+
+        while let Some(evt) = discover.next().await {
+            match evt {
+                AdapterEvent::DeviceAdded(addr) => {
+                    println!("{} {addr}", "Device added".green());
+                }
+                AdapterEvent::DeviceRemoved(addr) => {
+                    println!("{} {addr}", "Device removes".red());
+                }
+                _ => (),
+            }
+        }
+        Ok(())
     }
 
-    fn pair(&self) {
+    async fn pair(&self) {
         todo!()
     }
 }
 
 impl LinuxBLEAuthenticator { 
-    fn new() -> LinuxBLEAuthenticator {
-        LinuxBLEAuthenticator { }
+    async fn new() -> LinuxBLEAuthenticator {
+        let session_result = Session::new().await;
+
+        let session = match session_result {
+            Ok(session) => session,
+            Err(_) => {println!("An error occurred, exiting"); process::exit(1)},
+        };
+        LinuxBLEAuthenticator{session}
     }
 }
 
@@ -31,7 +65,11 @@ pub struct BLEAuthenticatorFactory {
 }
 
 impl BLEAuthenticatorFactory {
-    pub fn create(&self) -> Box<dyn BLEAuthenticator> {
-        Box::new(LinuxBLEAuthenticator::new())
+    pub fn new() -> BLEAuthenticatorFactory{
+        BLEAuthenticatorFactory{}
+    }
+
+    pub async fn create(&self) -> Box<dyn BLEAuthenticator> {
+        Box::new(LinuxBLEAuthenticator::new().await)
     }
 }
